c7eb9a47ae5d101cee4500a64123143a0f306382
rock's ezweb <alikerock@gmail.com>
2024-12-03 11:46:50 +0900

    로그인 페이지 수정

--- header.php
+++ header.php
@@ -59,7 +59,7 @@
 		</form>
 		<div>
 			<?php if ( is_user_logged_in() ) : ?>
-				<a href="<?php wp_logout_url() ?>">로그아웃</a>
+				<a href="<?php echo wp_logout_url() ?>">로그아웃</a>
 			<?php else :?>
 				<a href="<?php bloginfo('url') ?>/login/">로그인</a>
 				<a href="<?php bloginfo('url') ?>/wp-login.php?action=register">회원가입</a>
